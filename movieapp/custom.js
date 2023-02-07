1eff735eae117f3f44234dc0599b491e771c4169
OK-Emmanuel <olawuniemmanuelk@gmail.com>
2023-02-07 13:17:58 +0100

    Movie App Interface withAPI

--- movieapp/custom.js
+++ movieapp/custom.js
@@ -25,7 +25,13 @@ function showMovies(movies){
         movieEl.innerHTML = `
         <img src="${IMG_PATH + poster_path}" alt="${title}">
         <div class="movie-info"> 
-        <h3> ${title}</h3>
-        <span class="${getClassByRate(vote_average)}"</div>`
+            <h3> ${title}</h3>
+            <span class="${getClassByRate(vote_average)}"> ${vote_average}<span>
+        </div>
+
+        <div class="overview">
+        <h3>Overview</h3>
+        ${overview}`
+
     })
 }
